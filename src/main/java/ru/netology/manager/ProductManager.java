26fec9a870b00f104bcde225ad51838df913c5be
roslsann <sann23@mail.ru>
2022-02-22 16:20:29 +0300

    Manager added

---
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -0,0 +1,45 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Product;
+import ru.netology.domain.Book;
+import ru.netology.domain.Smartphone;
+import ru.netology.repository.ProductRepository;
+
+public class ProductManager {
+    ProductRepository repository;
+
+    ProductManager(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public Product[] searchBy(String text) {
+        Product[] result = new Product[0];
+        for (Product product : repository.findAll()) {
+            if (matches(product, text)) {
+                Product[] tmp = new Product[result.length + 1];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                tmp[tmp.length - 1] = product;
+                result = tmp;
+            }
+        }
+        return result;
+    }
+
+    public boolean matches(Product product, String search) {
+        if (product instanceof Book) {
+            Book book = (Book) product;
+            if (book.getAuthor().contains(search)) {
+                return true;
+            }
+            return book.getName().contains(search);
+        }
+        if (product instanceof Smartphone) {
+            Smartphone smartphone = (Smartphone) product;
+            if (smartphone.getManufacturer().contains(search)) {
+                return true;
+            }
+            return smartphone.getName().contains(search);
+        }
+        return false;
+    }
+}
---
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -0,0 +1,70 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Book;
+import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
+import ru.netology.repository.ProductRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductMangerTest {
+    static ProductRepository repo = new ProductRepository(); //создал экземпляр репозитория
+    ProductManager manager = new ProductManager(repo); //создал экземпляр менеджера и передал в параметры репозиторий
+    static Product smartphone1 = new Smartphone(1, "S21", 700, "Samsung");//создаю экземпляры товаров
+    static Product smartphone2 = new Smartphone(5, "iPhone12", 1000, "Apple");
+    static Product book1 = new Book(5, "Война и Мир", 1000, "Толстой");
+    static Product book2 = new Book(1, "Преступление и наказание", 500, "Достоевский");
+    static Product book3 = new Book(3, "Фауст", 700, "Гете");
+
+    @BeforeAll
+    static void setUp() {
+        repo.save(smartphone1);
+        repo.save(smartphone2);
+        repo.save(book1);
+        repo.save(book2);
+        repo.save(book3);
+    }
+
+    @Test
+    public void shouldFindBookName() {
+        Product[] expected = {book1};
+        Product[] actual = manager.searchBy("Война и Мир");
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindBookAuthor() {
+        Product[] expected = {book2};
+        Product[] actual = manager.searchBy("Достоевский");
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindSmartphoneManufacturer() {
+        Product[] expected = {smartphone1};
+        Product[] actual = manager.searchBy("Samsung");
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindSmartphoneName() {
+        Product[] expected = {smartphone1};
+        Product[] actual = manager.searchBy("S21");
+
+        assertArrayEquals(expected, actual);
+    }
+
+    /*@Test
+    public void shouldFindTheSameText() {
+        Product[] expected = {book1, book2};
+        Product[] actual = manager.searchBy("Преступление и наказание");
+
+        assertArrayEquals(expected, actual);
+    } */
+
+}
