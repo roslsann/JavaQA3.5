3e3b12250a328a4f15b3bfbf589d76eaa0b68324
roslsann <sann23@mail.ru>
2022-02-23 20:01:45 +0300

    Test modified

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -9,19 +9,22 @@ import ru.netology.repository.ProductRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ProductMangerTest {
+class ProductManagerTest {
     static ProductRepository repo = new ProductRepository();
     ProductManager manager = new ProductManager(repo);
     static Product smartphone1 = new Smartphone(1, "S21", 700, "Samsung");
     static Product smartphone2 = new Smartphone(5, "iPhone12", 1000, "Apple");
+    static Product smartphone3 = new Smartphone(7, "iPhone11", 900, "Apple");
     static Product book1 = new Book(5, "Война и Мир", 1000, "Толстой");
     static Product book2 = new Book(1, "Преступление и наказание", 500, "Достоевский");
     static Product book3 = new Book(3, "Фауст", 700, "Гете");
+
 
     @BeforeAll
     static void setUp() {
         repo.save(smartphone1);
         repo.save(smartphone2);
+        repo.save(smartphone3);
         repo.save(book1);
         repo.save(book2);
         repo.save(book3);
@@ -55,6 +58,22 @@ class ProductMangerTest {
     public void shouldFindSmartphoneName() {
         Product[] expected = {smartphone1};
         Product[] actual = manager.searchBy("S21");
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindSmartphoneIfSameManufacturer() {
+        Product[] expected = {smartphone2, smartphone3};
+        Product[] actual = manager.searchBy("Apple");
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindSmartphone() {
+        Product[] expected = {};
+        Product[] actual = manager.searchBy("Xiaomi");
 
         assertArrayEquals(expected, actual);
     }
